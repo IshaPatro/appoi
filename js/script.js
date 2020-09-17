cf6b6a7119ad1d25eccb87f6ed6747ab550d325a
Isha Patro <isha.patro.215@gmail.com>
2020-09-17 13:50:34 +0530

    final

--- js/script.js
+++ js/script.js
@@ -118,20 +118,3 @@ function ChangeDepartment() {
     if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
   }
 }
-//--------------------------------------------------------------- FAQ-----------------------------------------------------------
-const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
-
-accordionItemHeaders.forEach(accordionItemHeader => {
-  accordionItemHeader.addEventListener("click", event => {
-
-    accordionItemHeader.classList.toggle("active");
-    const accordionItemBody = accordionItemHeader.nextElementSibling;
-    if(accordionItemHeader.classList.contains("active")) {
-      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
-    }
-    else {
-      accordionItemBody.style.maxHeight = 0;
-    }
-
-  });
-});
